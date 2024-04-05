6822a263ff02e9ca12f44f638f14130fc17f7123
ryanpham540 <ryanpham20@gmail.com>
2024-04-05 10:42:55 -0700

    changes made on thursday class

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('scrollButton').addEventListener('click', function() {
+    var container = document.querySelector('.horizontal-scroll-section');
+    container.scrollLeft += 200; // Adjust scrollAmount as needed
+   });
+   
